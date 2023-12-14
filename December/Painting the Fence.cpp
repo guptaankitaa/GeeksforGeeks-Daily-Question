ae32d5afeb1db0c0e6ddaba00ad1e8b2ec385013
Ankita Gupta <112807231+guptaankitaa@users.noreply.github.com>
2023-12-14 22:22:33 +0530

    Create Painting the Fence.cpp

---
+++ December/Painting the Fence.cpp
@@ -0,0 +1,14 @@
+class Solution{
+    public:
+    long long countWays(int n, int k){
+        if(n==1)return k;
+        long long same=k,diff=1LL*k*(k-1),total=same+diff,m=1e9+7;
+        for(int i=3;i<=n;i++){
+            same=diff;
+            diff=total*(k-1)%m;
+            total=(same+diff)%m;
+        }
+        return total;
+    }
+};
+
